5867d9531900043fa2f7edff973a7d96ad2239c3
DJONY S NUNES <djony2003@gmail.com>
2023-03-21 20:06:48 -0300

    2103

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-import  { createDiv, createCheck, createText, createButton } from './tarefa.js';
+import { createDiv, createCheck, createText, createButton } from "./tarefa.js";
 
-
-window._addTarefa = () => { 
-
+window._addTarefa = () => {
   const _inText = document.querySelector(".inputText");
   const _inList = document.querySelector("._listTarefas");
   const _div = createDiv(_inList);
-  const _check = createCheck(_div);
+  // const _check = createCheck(_div);
   const texto = _inText.value;
   const _text = createText(_div, texto);
   const _btn = createButton(_div);
 
-  
+  const _listTar = {
+    _div,  
+    _text,
+    _btn,
+  };
+
   _inText.value = "";
-  
+
   _btn.addEventListener("click", () => {
     _div.remove();
   });
+
+  console.log(_listTar);
 };
